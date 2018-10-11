a27b003a775e2474bb70cae3133eaca24ed90ec5
Nunu <yoonho12345@naver.com>
2018-10-11 17:36:02 +0900

    some print related methods. variables

---
+++ Javastudy/2018_10_11/TrainingEx04.java
@@ -0,0 +1,10 @@
+
+public class TrainingEx04 {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("%.2f\n", 25.8978);
+
+	}
+
+}
---
+++ Javastudy/2018_10_11/TrainingEx05.java
@@ -0,0 +1,10 @@
+
+public class TrainingEx05 {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("예금자: %s\n송금되기전 잔금: %d원\n송금된 금액: %,d원\n송금이후 잔금: %,d원\n내일 입급될이자: %,.1f원\n", "유재석", 1000, 325, 1325, 1397.5);
+
+	}
+
+}
